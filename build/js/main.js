214d337b7ced42fbe98d2ee23f20729ac3821945
dirkraft <dtforweb@gmail.com>
2021-09-28 23:12:15 +0300

    start mobile

--- build/js/main.js
+++ build/js/main.js
@@ -7,14 +7,18 @@ chart.geodataNames = am4geodata_lang_RU;
 chart.geodata = am4geodata_franceHigh;
 
 chart.projection = new am4maps.projections.Miller();
+chart.seriesContainer.draggable = false;
 chart.seriesContainer.resizable = false;
+chart.seriesContainer.wheelable = false;
 
 let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 polygonSeries.useGeodata = true;
 
 let polygonTemplate = polygonSeries.mapPolygons.template;
+console.log('ee',polygonTemplate);
 polygonTemplate.tooltipText = "{name}";
 polygonTemplate.fill = am4core.color("#477A6C");
+polygonTemplate.wheelable = false;
 let hs = polygonTemplate.states.create("hover");
 hs.properties.fill = am4core.color("#FED3C3");
 polygonSeries.mapPolygons.template.events.on("hit", function(ev) {
