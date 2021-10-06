44bdac90e4d32449f6117b8f7ab9c0ef334984ab
dirkraft <dtforweb@gmail.com>
2021-10-06 13:28:20 +0300

    fix zoom to map

--- build/js/main.js
+++ build/js/main.js
@@ -23,7 +23,21 @@ hs.properties.fill = am4core.color("#FED3C3");
 polygonSeries.mapPolygons.template.events.on("hit", function(ev) {
 
 });
-document.addEventListener('mousemove', function(ev){
-  document.getElementsByClassName('section-video__preview').style.transform = 'translateY('+(ev.clientY-80)+'px)';
-  document.getElementsByClassName('section-video__preview').style.transform += 'translateX('+(ev.clientX-100)+'px)';
-},false);
+let videoPreview = document.querySelector('.section-video__preview');
+let videoPreviewBtn = document.querySelector('.section-video__play');
+
+videoPreview.addEventListener('mousemove', function (event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  videoPreviewBtn.style.left += `${x}px`;
+  videoPreviewBtn.style.top += `${y}px`;
+}, false);
+
+videoPreview.addEventListener('mouseout', function (event) {
+  videoPreviewBtn.classList.remove('focused');
+}, false);
+
+// document.addEventListener('mousemove', function(ev){
+//   document.getElementsByClassName('section-video__preview').style.transform = 'translateY('+(ev.clientY-80)+'px)';
+//   document.getElementsByClassName('section-video__preview').style.transform += 'translateX('+(ev.clientX-100)+'px)';
+// },false);
--- src/js/mouseEvents.js
+++ src/js/mouseEvents.js
@@ -1,4 +1,18 @@
-document.addEventListener('mousemove', function(ev){
-  document.getElementsByClassName('section-video__preview').style.transform = 'translateY('+(ev.clientY-80)+'px)';
-  document.getElementsByClassName('section-video__preview').style.transform += 'translateX('+(ev.clientX-100)+'px)';
-},false);
+let videoPreview = document.querySelector('.section-video__preview');
+let videoPreviewBtn = document.querySelector('.section-video__play');
+
+videoPreview.addEventListener('mousemove', function (event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  videoPreviewBtn.style.left += `${x}px`;
+  videoPreviewBtn.style.top += `${y}px`;
+}, false);
+
+videoPreview.addEventListener('mouseout', function (event) {
+  videoPreviewBtn.classList.remove('focused');
+}, false);
+
+// document.addEventListener('mousemove', function(ev){
+//   document.getElementsByClassName('section-video__preview').style.transform = 'translateY('+(ev.clientY-80)+'px)';
+//   document.getElementsByClassName('section-video__preview').style.transform += 'translateX('+(ev.clientX-100)+'px)';
+// },false);
