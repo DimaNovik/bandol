032533e2ed7bfad8a61f9eecb3ec460dfdc63061
dirkraft <dtforweb@gmail.com>
2021-10-15 17:40:34 +0300

    add logic to slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 // Импортируем другие js-файлы
-//= locomotive.min.js
 //= map.js
 //= slider.js
 //= mouseEvents.js
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,63 @@
 const swiper = new Swiper(".slider", {
-
+  preloadImages: false,
+  lazy: true,
 });
+
+swiper.on('slideChange', function () {
+  changeSliderInfo()
+});
+
+let sliderInfoItem = document.querySelectorAll('.slider-info__item');
+
+(function getSliderItems(){
+  let sliderListItem = document.querySelectorAll('.slider-list__link');
+  let sliderBtnRight = document.querySelector('.slider-info__btn--right');
+  let sliderBtnLeft = document.querySelector('.slider-info__btn--left');
+  
+  setSliderLengthText(sliderListItem.length);
+  
+  sliderBtnRight.addEventListener('click', function () {
+    incrementSlider(sliderListItem.length)
+  });
+  
+  sliderBtnLeft.addEventListener('click', function () {
+    decrementSlider()
+  });
+  
+  sliderListItem.forEach((item, key) => {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+      setActiveSlider(key)
+    }, false)
+  });
+})();
+
+function changeSliderInfo() {
+  sliderInfoItem.forEach((item, key) => {
+  
+  })
+}
+
+function incrementSlider(length) {
+  if(length === swiper.activeIndex) {
+    return;
+  }
+  swiper.slideTo(swiper.activeIndex + 1);
+}
+
+function decrementSlider() {
+  if(swiper.activeIndex === 0) {
+    return;
+  }
+  swiper.slideTo(swiper.activeIndex - 1);
+}
+
+function setSliderLengthText(length) {
+  let sliderCount = document.getElementById('slider-length');
+  sliderCount.innerHTML = length  < 10 ? `0${length}` : length;
+}
+
+
+function setActiveSlider(key) {
+  swiper.slideTo(key);
+}
